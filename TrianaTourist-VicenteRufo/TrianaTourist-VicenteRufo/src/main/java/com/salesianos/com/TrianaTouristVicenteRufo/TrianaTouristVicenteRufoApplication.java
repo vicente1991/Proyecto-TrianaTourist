4b2fb4eaeb77baf765f4cf0231dc3d994a89fae3
vicente <etenciv@gmail.com>
2021-12-17 13:24:25 +0100

    Inicio proyecto

---
+++ TrianaTourist-VicenteRufo/TrianaTourist-VicenteRufo/src/main/java/com/salesianos/com/TrianaTouristVicenteRufo/TrianaTouristVicenteRufoApplication.java
@@ -0,0 +1,13 @@
+package com.salesianos.com.TrianaTouristVicenteRufo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrianaTouristVicenteRufoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrianaTouristVicenteRufoApplication.class, args);
+	}
+
+}
---
+++ TrianaTourist-VicenteRufo/TrianaTourist-VicenteRufo/src/test/java/com/salesianos/com/TrianaTouristVicenteRufo/TrianaTouristVicenteRufoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.salesianos.com.TrianaTouristVicenteRufo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrianaTouristVicenteRufoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
